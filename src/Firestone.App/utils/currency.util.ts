530548f707fa19af753bb05351cce0cba34041d8
William Pei <draekien@users.noreply.github.com>
2022-08-13 23:44:27 +1000

    feat: begin styling tables page

--- src/Firestone.App/utils/currency.util.ts
+++ src/Firestone.App/utils/currency.util.ts
@@ -2,7 +2,9 @@ export const stringOrNumberAsCurrency = (value: string | number): string => {
   return value.toLocaleString("en-AU", { style: "currency", currency: "AUD" });
 };
 
-export const numberAsCurrency = (value: number): string => {
+export const numberAsCurrency = (value?: number): string => {
+  if (!value) return "-";
+
   return new Intl.NumberFormat("en-AU", {
     style: "currency",
     currency: "AUD",
